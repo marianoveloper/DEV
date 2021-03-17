a31511ef800e08299e9606e286d0daee8d28a4b0
mariano <benitezmariano5@gmail.com>
2021-03-17 10:09:15 -0300

    jetstream

---
+++ eduvirtual-v2/resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>How to install Botman Chatbot in Laravel? - Tutsmake.org</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+    </head>
+    <body>
+
+    </body>
+
+
+
+
+  <script>
+var botmanWidget = {
+    frameEndpoint: '/botman/chat',
+    title:"Educación Virtual",
+     introMessage: 'Bienvenido a Uccuyo Virtual',
+     bubbleAvatarUrl:"images/perrito.png",
+};
+</script>
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+
+</html>
