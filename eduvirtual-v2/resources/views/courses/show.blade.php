aa56c36f6c94e18603b4d0aa5c4cd3721d44288a
Mariano <benitezmariano5@gmail.com>
2021-08-17 11:46:56 -0300

    vista show

--- eduvirtual-v2/resources/views/courses/show.blade.php
+++ eduvirtual-v2/resources/views/courses/show.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
-    <section class="py-10 mb-8 bg-green-900 ">
-        <div class="container ">
-            <div>
+
+
+    <section class="py-10 mb-8" style="background-image: url({{asset('images/homes/barra-verde.png')}})">
+        <div class="container grid grid-cols-3">
+            <div class="col-span-2">
 
                 <div class="text-white ">
                     <h1 class="text-5xl">{{$course->title}}</h1>
@@ -11,181 +13,117 @@
                     Abiertas</span>
 
             </div>
-
         </div>
 
     </section>
 
+    <div class="container grid grid-cols-1 gap-6 lg:grid-cols-3">
 
-    <div class="container grid grid-cols-3">
+        <div class="order-2 lg:col-span-2 lg-order-1">
+            <div class="col-span-2">
+                <section class="mb-12">
 
-    <div class="mx-auto md:fixed right-20 top-20">
-        <article class="overflow-hidden bg-white rounded shadow-lg ">
-            <img class="object-contain w-full h-64 " src="{{Storage::url($course->image->url)}}" alt="">
+                    <h1 class="mb-2 text-3xl font-bold">Presentación</h1>
+                    <div class="text-base text-gray-700">{!! html_entity_decode($course->description) !!}</div>
 
 
-            <div class="px-6 py-4">
+                </section>
+                <section>
+                    <article class="mb-4 shadow" x-data="{ open: false }">
+                        <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
+                            x-on:click="open = !open">
+                            <h1 class="text-lg font-bold text-gray-600">Objetivos</h1>
+                        </header>
+                        <div class="px-4 py-2 bg-white" x-show="open">
+                            <ul>
+                                @foreach($course->goals as $goal)
+                                <li class="text-base text-gray-700"><i
+                                        class="mr-2 text-gray-600 fas fa-check"></i>{{$goal->name}}</li>
+                                @endforeach
 
-                <p class="mb-2 text-gray-500 text-md">Inicio: {{$course->date_start}}</p>
-                <p class="mb-2 text-gray-500 text-md">Precio: ${{$course->price}}</p>
-                <p class="mb-2 text-gray-500 text-md">Duracion: 3 meses </p>
-                <a href="{{$course->link_inscription}}"
-                    class="block px-8 py-3 mt-4 text-center text-white bg-yellow-500 border rounded hover:border-gray-500 hover:bg-white hover:text-green-900">
-                    PreInscripcion
-                </a>
-                <a href="{{$course->url_info}}"
-                    class="block px-8 py-3 mt-4 text-center text-green-900 border border-green-900 rounded hover:border-white hover:bg-yellow-500 hover:text-white">Informativo</a>
+
+                            </ul>
+                        </div>
+                    </article>
+                </section>
+                <section>
+
+                    <article class="mb-4 shadow" x-data="{ open: false }">
+                        <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
+                            x-on:click="open = !open">
+                            <h1 class="text-lg font-bold text-gray-600">Destinatarios</h1>
+                        </header>
+                        <div class="px-4 py-2 bg-white" x-show="open">
+                            <p class="text-base text-gray-700">{{$course->destination}}</p>
+                        </div>
+                    </article>
+
+
+                </section>
+
+                <section>
+                    <article class="mb-4 shadow" x-data="{ open: false }">
+                        <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
+                            x-on:click="open = !open">
+                            <h1 class="text-lg font-bold text-gray-600">Requisitos</h1>
+                        </header>
+                        <div class="px-4 py-2 bg-white" x-show="open">
+                            <ul>
+                                @forelse($course->requirements as $requirement)
+                                <li class="text-base text-gray-700">{{$requirement->name}}</li>
+
+                                @empty
+                                <li>Sin Requisitos </li>
+                                @endforelse
+                            </ul>
+                        </div>
+                    </article>
+                </section>
+                <section>
+                    <article class="mb-4 shadow" x-data="{ open: false }">
+                        <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
+                            x-on:click="open = !open">
+                            <h1 class="text-lg font-bold text-gray-600">Resoluciones</h1>
+                        </header>
+                        <div class="px-4 py-2 bg-white" x-show="open">
+                            <ul>
+                                @forelse($course->requirements as $requirement)
+                                <li class="text-base text-gray-700"><i
+                                        class="mr-2 fas fa-play-circle"></i>{{$requirement->name}}</li>
+
+                                @empty
+                                <li>Sin Requisitos </li>
+                                @endforelse
+                            </ul>
+                        </div>
+                    </article>
+                </section>
+
+
             </div>
-        </article>
-
-    </div>
-        <div class="col-span-2">
-            <section class="mb-12">
-
-
-                <h1 class="mb-2 text-3xl font-bold">Presentación</h1>
-                <div class="text-base text-gray-700">{!! html_entity_decode($course->description) !!}</div>
-
-
-            </section>
-            <section>
-                <article class="mb-4 shadow" x-data="{ open: false }">
-                    <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
-                        x-on:click="open = !open">
-                        <h1 class="text-lg font-bold text-gray-600">Objetivos</h1>
-                    </header>
-                    <div class="px-4 py-2 bg-white" x-show="open">
-                        <ul>
-                            @foreach($course->goals as $goal)
-                            <li class="text-base text-gray-700">{{$goal->name}}</li>
-                            @endforeach
-
-
-                        </ul>
-                    </div>
-                </article>
-            </section>
-            <section>
-
-                <article class="mb-4 shadow" x-data="{ open: false }">
-                    <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
-                        x-on:click="open = !open">
-                        <h1 class="text-lg font-bold text-gray-600">Destinatarios</h1>
-                    </header>
-                    <div class="px-4 py-2 bg-white" x-show="open">
-                        <p class="text-base text-gray-700">{{$course->destination}}</p>
-                    </div>
-                </article>
-
-
-            </section>
-
-            <section>
-                <article class="mb-4 shadow" x-data="{ open: false }">
-                    <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
-                        x-on:click="open = !open">
-                        <h1 class="text-lg font-bold text-gray-600">Requisitos</h1>
-                    </header>
-                    <div class="px-4 py-2 bg-white" x-show="open">
-                        <ul>
-                            @forelse($course->requirements as $requirement)
-                            <li class="text-base text-gray-700">{{$requirement->name}}</li>
-
-                            @empty
-                            <li>Sin Requisitos </li>
-                            @endforelse
-                        </ul>
-                    </div>
-                </article>
-            </section>
-            <section>
-                <article class="mb-4 shadow" x-data="{ open: false }">
-                    <header class="px-4 py-2 bg-gray-200 border border-gray-200 cursor-pointer"
-                        x-on:click="open = !open">
-                        <h1 class="text-lg font-bold text-gray-600">Resoluciones</h1>
-                    </header>
-                    <div class="px-4 py-2 bg-white" x-show="open">
-                        <ul>
-                            @forelse($course->requirements as $requirement)
-                            <li class="text-base text-gray-700">{{$requirement->name}}</li>
-
-                            @empty
-                            <li>Sin Requisitos </li>
-                            @endforelse
-                        </ul>
-                    </div>
-                </article>
-            </section>
-
-            <section class="mt-16 mb-12">
-
-
-                <h1 class="mb-2 text-3xl font-bold">Docentes</h1>
-
-                <!-- component -->
-                <!-- post card -->
-                <div class="flex mx-4 my-8 bg-white rounded-lg shadow-lg max-w sm:w-full ">
-                    <!--horizantil margin is just for display-->
-                    <div class="flex items-start px-4 py-6">
-                        <img class="object-cover w-16 h-16 mr-4 rounded-full shadow"
-                            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                            alt="avatar">
-                        <div class="">
-                            <div class="flex items-center justify-between">
-                                <h2 class="-mt-1 text-lg font-semibold text-gray-900">Brad Adams </h2>
-                                <small class="text-sm text-gray-700">22h ago</small>
-                            </div>
-                            <p class="text-gray-700">Joined 12 SEP 2012. </p>
-                            <p class="hidden mt-3 text-sm text-gray-700 md:block">
-                                Lorem ipsum, dolor sit amet conse. Saepe optio minus rem dolor sit amet!
-                            </p>
-
-                        </div>
-                    </div>
+        </div>
+        <div class="order-1 lg:order-2">
+            <div class="card md:fixed md:right-20 md:top-20">
+                <div class="flex">
+                    <img class="object-cover w-full h-64 rounded shadow-lg" src="{{Storage::url($course->image->url)}}"
+                        alt="">
                 </div>
-                <!-- component -->
-                <!-- post card -->
-                <div class="flex max-w-md mx-4 my-8 bg-white rounded-lg shadow-lg md:mx-auto md:max-w-2xl ">
-                    <!--horizantil margin is just for display-->
-                    <div class="flex items-start px-4 py-6">
-                        <img class="object-cover w-12 h-12 mr-4 rounded-full shadow"
-                            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                            alt="avatar">
-                        <div class="">
-                            <div class="flex items-center justify-between">
-                                <h2 class="-mt-1 text-lg font-semibold text-gray-900">Brad Adams </h2>
-                                <small class="text-sm text-gray-700">22h ago</small>
-                            </div>
-                            <p class="text-gray-700">Joined 12 SEP 2012. </p>
-                            <p class="mt-3 text-sm text-gray-700 ">
-                                Lorem ipsum, dolor sit amet conse. Saepe optio minus rem dolor sit amet!
-                            </p>
 
-                        </div>
-                    </div>
-                </div>
-                <!-- component -->
-                <!-- post card -->
-                <div class="flex max-w-md mx-4 my-8 bg-white rounded-lg shadow-lg md:mx-auto md:max-w-2xl ">
-                    <!--horizantil margin is just for display-->
-                    <div class="flex items-start px-4 py-6">
-                        <img class="object-cover w-12 h-12 mr-4 rounded-full shadow"
-                            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                            alt="avatar">
-                        <div class="">
-                            <div class="flex items-center justify-between">
-                                <h2 class="-mt-1 text-lg font-semibold text-gray-900">Brad Adams </h2>
-                                <small class="text-sm text-gray-700">22h ago</small>
-                            </div>
-                            <p class="text-gray-700">Joined 12 SEP 2012. </p>
-                            <p class="mt-3 text-sm text-gray-700">
-                                Lorem ipsum, dolor sit amet conse. Saepe optio minus rem dolor sit amet!
-                            </p>
+                <div class="px-6 py-4">
 
-                        </div>
-                    </div>
+                    <p class="mb-2 text-gray-500 text-md">Inicio: {{$course->date_start}}</p>
+                    <p class="mb-2 text-gray-500 text-md">Precio: ${{$course->price}}</p>
+                    <p class="mb-2 text-gray-500 text-md">Duracion: 3 meses </p>
+                    <a href="{{$course->link_inscription}}"
+                        class="block px-8 py-3 mt-4 text-center text-white bg-yellow-500 border rounded hover:border-gray-500 hover:bg-white hover:text-green-900">
+                        PreInscripcion
+                    </a>
+                    <a href="{{$course->url_info}}"
+                        class="block px-8 py-3 mt-4 text-center text-green-900 border border-green-900 rounded hover:border-white hover:bg-yellow-500 hover:text-white">Informativo</a>
                 </div>
-            </section>
+
+
+            </div>
         </div>
 
     </div>
