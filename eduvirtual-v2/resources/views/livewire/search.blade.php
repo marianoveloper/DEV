24a025c6913ec776dd4dabc024f226ce80bca6e9
Mariano <benitezmariano5@gmail.com>
2021-08-31 21:47:05 -0300

    arreglo buscador

--- eduvirtual-v2/resources/views/livewire/search.blade.php
+++ eduvirtual-v2/resources/views/livewire/search.blade.php
@@ -4,12 +4,12 @@
         type="search" name="search" placeholder="¿Qué querés estudiar?">
 
     <button type="submit"
-        class="absolute top-0 right-0 z-50 px-4 py-2 mt-2 font-bold bg-yellow-500 rounded hover:bg-yellow-700">
+        class="absolute top-0 right-0 px-4 py-2 mt-2 font-bold bg-yellow-500 rounded hover:bg-yellow-700">
         <x-search size="21" color="white" />
     </button>
     @if($search)
 
-        <ul class="absolute w-full mt-1 overflow-hidden bg-white rounded-lg z-50-left-0">
+        <ul class="absolute z-50 w-full mt-1 overflow-hidden bg-white rounded-lg z-50-left-0">
             @forelse($this->results as $result)
             <a href="{{route('courses.show',$result)}}">
                 <li class="px-5 text-sm leading-10 cursor-pointer hover:bg-gray-300">
