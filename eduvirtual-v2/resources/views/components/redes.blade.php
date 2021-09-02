c0cd63ac2c25287bcbbe0f8e7555ff032e8eb46d
mariano <benitezmariano5@gmail.com>
2021-09-02 20:50:56 -0300

    redes

---
+++ eduvirtual-v2/resources/views/components/redes.blade.php
@@ -0,0 +1,14 @@
+	<!-- End Header-->
+	<div id="fixed-social">
+		<div>
+		  <a href="https://www.facebook.com/UCCuyoEducacionVirtual" class="fixed-linkedin" target="_blank"><i class="fab fa-facebook-f"></i> <span>Facebook</span></a>
+		</div>
+		<div>
+			<a href="https://www.instagram.com/uccuyoeducacionvirtual/" class="fixed-linkedin" target="_blank"><i class="fab fa-instagram"></i> <span>Instagram</span></a>
+		  </div>
+
+		<div>
+		  <a href="https://www.linkedin.com/in/uccuyoeducacionvirtual/" class="fixed-linkedin" target="_blank"><i class="fab fa-linkedin-in"></i> <span>Linkedin</span></a>
+		</div>
+	  </div>
+	<!-- intro -->
--- eduvirtual-v2/resources/views/welcome.blade.php
+++ eduvirtual-v2/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-
+<x-redes/>
     <section class="bg-cover" style="background-image: url({{asset('images/homes/portada-19b.png')}})">
         <div class="px-4 max-w-7x1 max-auto sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
